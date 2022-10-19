a346c3437cca3f441abe70ffeac400307fa5b2ff
Sergio Andrés Jolivet <andresjo2014@gmail.com>
2022-10-19 18:45:05 -0300

    Test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Business;
+using System;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,45 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            while (true)
+            {
+                Console.WriteLine("Enter full path of Salary files");
+                string strPath = Console.ReadLine();
+
+                try
+                {
+                    var lst = new SalaryEmployeeHandler(strPath).ProccesFiles();
+
+                    foreach (Employee employee in lst)
+                    {
+                        Console.WriteLine("Employee Name: " + employee.FullName + " Salary: " + employee.SalaryCalculated.ToString());
+                        Console.WriteLine(Environment.NewLine);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error Execption-----------");
+                    Console.WriteLine(Environment.NewLine);
+                    Console.WriteLine(ex.Message);
+                }
+
+                Console.WriteLine("Do you want to contiue Y/N or different key to close the program");
+                string answer = Console.ReadLine();
+
+                if(answer!="Y")
+                {
+                    break;
+                }
+            }
+            
+
+
+            
         }
+
+
+
     }
 }
---
+++ ConsoleApp1/Readme.txt
@@ -0,0 +1,2 @@
+1 Create path C:/Employees and copy the file SalaryMonthlyPayment.txt and other files that you want to test.
+2 Paste the path in the console when si required
